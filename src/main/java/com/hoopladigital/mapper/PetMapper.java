a7797e3e6f79d7bda42068ad78bb3595edd312b2
Egor Gerasimenko <egor.gerasimenko@robotsandpencils.com>
2019-03-25 00:09:18 -0600

    Minor bug fix in PetMapper

--- src/main/java/com/hoopladigital/mapper/PetMapper.java
+++ src/main/java/com/hoopladigital/mapper/PetMapper.java
@@ -1,12 +1,13 @@
 package com.hoopladigital.mapper;
 
 import com.hoopladigital.bean.Pet;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface PetMapper {
 	List<Pet> getPetList(Long personId);
-	Pet getPet(Long personId, Long petId);
+	Pet getPet(@Param("personId") Long personId, @Param("petId") Long petId);
 	int insertPet(Pet pet);
 	int updatePet(Pet pet);
 	int deletePet(Long petId);
